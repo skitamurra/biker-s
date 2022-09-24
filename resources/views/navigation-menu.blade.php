cc6cd2a3d65ae1e890a26ff33655ffe24049ee13
s.kitamura <s.g004baa@gmail.com>
2022-09-24 17:47:47 +0900

    add create function

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -15,6 +15,9 @@
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-jet-nav-link>
+                    <x-nav-link :href="route('index')" :active="request()->routeIs('index')">
+                        {{ __('Index') }}
+                    </x-nav-link>
                 </div>
             </div>
 
